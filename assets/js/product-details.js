de166d8db49dfee786f43176b30a7af45e090722
bechmoumnouh-del <bechmoumnouh@gmail.com>
2026-08-07 02:46:30 +0100

    Update product-details.js

--- assets/js/product-details.js
+++ assets/js/product-details.js
@@ -69,7 +69,7 @@ const PRODUCT_DETAILS = {
       id: 1,
       qty: 1,
       price: 3490,
-      freeDelivery: false,
+      freeDelivery: true,
       title: 'قطعة واحدة',
       active: true, // pre-selected on page load
     },
